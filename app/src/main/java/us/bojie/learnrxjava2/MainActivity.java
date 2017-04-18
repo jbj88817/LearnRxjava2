7cc70d0cb8de6e52ce140bf67cf4086032ac7c83
Bojie Jiang <jiangbojie@gmail.com>
2017-04-17 21:48:35 -0700

    basic use

--- app/src/main/java/us/bojie/learnrxjava2/MainActivity.java
+++ app/src/main/java/us/bojie/learnrxjava2/MainActivity.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.concurrent.Callable;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -19,24 +23,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.tv_text);
     }
 
     public void click(View view) {
         Observable<String> observable = getObservable();
-        Observer<String> observer = getObserver();
-        observable.subscribe(observer);
+//        Observer<String> observer = getObserver();
+//        observable.subscribe(observer);
+        observable.subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.d(TAG, "accept: " + s);
+                mTextView.append(s);
+                mTextView.append("\n");
+            }
+        });
+
     }
 
     public Observable<String> getObservable() {
-        return Observable.create(new ObservableOnSubscribe<String>() {
+
+//        return Observable.just("dabaojian", "yedian", "liaomei");
+//        return Observable.fromArray("dabaojian", "yedian", "liaomei");
+        return Observable.fromCallable(new Callable<String>() {
             @Override
-            public void subscribe(ObservableEmitter<String> e) throws Exception {
-                e.onNext("dabaojian");
-                e.onNext("yedian");
-                e.onComplete();
-//                e.onError(new );
+            public String call() throws Exception {
+                return "dabaojian";
             }
         });
+
+//        return Observable.create(new ObservableOnSubscribe<String>() {
+//            @Override
+//            public void subscribe(ObservableEmitter<String> e) throws Exception {
+//                e.onNext("dabaojian");
+//                e.onNext("yedian");
+//                e.onComplete();
+////                e.onError(new );
+//            }
+//        });
     }
 
     public Observer<String> getObserver() {
